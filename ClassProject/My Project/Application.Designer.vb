6a933b41f76af7e5a4dd2c9eb79ea553d1f66ce2
kreativesahara <technologieslensmen@gmail.com>
2025-11-17 10:46:41 +0300

    fix

--- ClassProject/My Project/Application.Designer.vb
+++ ClassProject/My Project/Application.Designer.vb
@@ -36,11 +36,6 @@ Namespace My
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Sub OnCreateSplashScreen()
-            Me.SplashScreen = Global.ClassProject.SplashScreen
-        End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Function OnInitialize(ByVal commandLineArgs As System.Collections.ObjectModel.ReadOnlyCollection(Of String)) As Boolean
             Me.MinimumSplashScreenDisplayTime = 0
             Return MyBase.OnInitialize(commandLineArgs)
